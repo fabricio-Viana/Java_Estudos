3408dc158910550dbb4b39f829628da7ce3308d8
Fabricio Viana <145355057+fabricio-Viana@users.noreply.github.com>
2024-11-23 15:47:09 -0300

    DesafioAritimetico

---
+++ src/Fundamentos/DesafioAritimetico.java
@@ -0,0 +1,28 @@
+
+package Fundamentos;
+
+public class DesafioAritimetico {
+    public static void main(String[] args) {
+         
+      int a = (1-5)*(2-7)/2;
+      int ma = (int) Math.pow(a,2);
+      
+       int b = (6*(3+2));
+       int mb = (int) Math.pow(b,2);
+       int f = 3*2;
+       int b2 = mb/f;
+       
+       int c = b2 - ma;
+       int mc =(int) Math.pow(c,3);
+       int dez = (int) Math.pow(10,3);
+       
+       int result = mc/dez;
+       
+       
+       
+        System.out.println(result);
+       
+        
+    }
+    
+}
