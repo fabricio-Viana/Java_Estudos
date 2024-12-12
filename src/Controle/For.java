a964ee79381ba11dde0e335c063ba6f7487f9d3b
fabricio-Viana <fabricio.viana.2301@gmail.com>
2024-12-12 16:45:49 -0300

    for

---
+++ src/Controle/For.java
@@ -0,0 +1,13 @@
+
+package Controle;
+
+
+public class For {
+    public static void main(String[] args) {
+        int n = 15;
+                
+          for(int i = 0; i < n ;i++ ){
+              System.out.println("TE amo mel");
+          }
+    }
+}
