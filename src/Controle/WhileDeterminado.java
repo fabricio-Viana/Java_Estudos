3fcc393d9c83e7cd761eea451e3e22b8e9175ca2
fabricio-Viana <fabricio.viana.2301@gmail.com>
2024-12-12 16:37:28 -0300

    while Determinado pelo valor da variavel e com incremento

---
+++ src/Controle/WhileDeterminado.java
@@ -0,0 +1,18 @@
+
+package Controle;
+
+
+public class WhileDeterminado {
+    
+    public static void main(String[] args) {
+        int n = 0;
+        
+        
+        while(n<10){
+            n++;
+            System.out.println("Bom dia");
+            
+        }
+    }
+    
+}
