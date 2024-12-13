4bab0df86746ec1c646cefcf3958d6ae18618113
fabricio-Viana <fabricio.viana.2301@gmail.com>
2024-12-13 15:25:43 -0300

    Desafio laço for onde não se pode usar tipos númericos para fazer um laço for

---
+++ src/Controle/DesafioFor.java
@@ -0,0 +1,14 @@
+
+package Controle;
+
+public class DesafioFor {
+    
+    public static void main(String[] args) {     
+        
+        for(String i = "#" ; !"##############".equals(i); i += "#"){
+            System.out.println(i);
+            
+        }
+    }
+    
+}
