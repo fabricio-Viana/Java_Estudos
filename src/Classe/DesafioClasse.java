2198087fb4152be0bbfb933e4d4db812b918da72
fabricio-Viana <fabricio.viana.2301@gmail.com>
2024-12-31 16:56:59 -0300

    Desafio De Classe

---
+++ src/Classe/DesafioClasse.java
@@ -0,0 +1,18 @@
+
+package Classe;
+
+public class DesafioClasse {
+    
+    int a = 3;
+    static int b = 4;
+    
+    public static void main(String[] args) {
+        
+        DesafioClasse p = new DesafioClasse();
+        
+        System.out.println(p.a);
+        
+        System.out.println(b);
+    }
+    
+}
