be2cc250fd8b49ccc07078f12b31b252837902c6
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-01-12 02:55:11 -0300

    Array2

--- src/ArrayEColections/Array1.java
+++ src/ArrayEColections/Array1.java
@@ -1,23 +1,40 @@
 
 package ArrayEColections;
-
+import java.util.Arrays;
 
 public class Array1 {
     public static void main(String[] args) {
-        
+        // array normal
         String[] nome = new String[5];
         
+        System.out.println(Arrays.toString(nome));
         
         nome[0] = "FABRICIO".concat(" Viana".toUpperCase());
         nome[1] = "Melissa".concat(" t vila".replace('t','A'));
-        
+        nome[2] = "João Tulio";
+        nome[3] = "Maria juliana";
+        nome[4] = "Pinto Junior";
+         
         System.out.println(nome[0]
                 .concat(" Ama muito sua esposa ")
                 .concat(nome[1]).toUpperCase());
+
+        System.out.println(Arrays.toString(nome));
+
+        //matriz
         
-        System.out.println( nome.equals(nome[0]));
-         
+        double[][] position = new double[3][3];
         
+        
+        for(int i = 0; i < nome.length;i++){
+            System.out.println(nome[i]);
+        }
+        
+        int[] decimal = {0,1,2,3,4,5,6,7,8,9};
+        
+        for(int i =0;i<decimal.length;i++){
+            System.out.println(decimal[i]);
+        }
     }
   
 }
