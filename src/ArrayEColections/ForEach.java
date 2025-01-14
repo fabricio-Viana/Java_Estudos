66b72b4cee964ca141cdc7262d9363d4fccb999c
fabricio-Viana <fabricio.viana.2301@gmail.com>
2025-01-14 01:53:14 -0300

    ForEach

---
+++ src/ArrayEColections/ForEach.java
@@ -0,0 +1,15 @@
+
+package ArrayEColections;
+
+public class ForEach {
+    public static void main(String[] args) {
+        Double[] a = {5.0,5.5,9.9,9.7,7.2,9.4};
+        
+        for(double x :a){
+            System.out.println(x);
+        }
+        
+        }
+        
+    }
+    
